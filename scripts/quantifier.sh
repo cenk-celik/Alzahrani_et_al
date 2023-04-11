5caae21ebdbf93298b01294ea23047dfd22817e0
_cenk <61861741+cenk-celik@users.noreply.github.com>
2023-04-11 16:48:26 +0800

    Update quantifier.sh

--- scripts/quantifier.sh
+++ scripts/quantifier.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-for fileName in data/IRE1_WT_{1..6};
-
-do
-
-samp=`basename ${fileName}`
-
-echo "Processing sample ${samp}"
-
-salmon quant -i index -l A \
-	-1 ${fileName}/${samp}_1.fq.gz \
-	-2 ${fileName}/${samp}_2.fq.gz \
-	-p 8 --validateMappings \
-	--gcBias -o quants/${samp}_quant
-done
-
-for fileName in data/IRE1_KO_{1..6};
+for fileName in data/IRE1_*_{1..6};
 
 do
 
